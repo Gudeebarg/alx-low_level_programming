52b2da86e6dd44a5c99aacaaa086ea3ebf31072d
Gudeebarg <chukwudigoodness288@gmail.com>
2023-07-13 15:14:31 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - A program that prints using the funtion printf
  * Return: 0 (Perfectly done)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+return (0);
 }
