d019b6f84d2b01220fcb5b040db05af216da042e
Gudeebarg <chukwudigoodness288@gmail.com>
2023-07-13 13:43:48 -0700

    printf

---
+++ 5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses printf to print
+ *Return: 0 (perfectly done)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
