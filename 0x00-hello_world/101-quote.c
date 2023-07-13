c8c832582b918af89dcffd02328cad126dd45eb2
Gudeebarg <chukwudigoodness288@gmail.com>
2023-07-13 15:07:52 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - UNIX program printing using printf
- * Return: 1 (Perfectly done)
+ * main - Entry point
+ * Return: 0 (Perfectly done)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
