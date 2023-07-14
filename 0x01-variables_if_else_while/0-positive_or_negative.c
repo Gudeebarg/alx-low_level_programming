b73b82a0feb35b89ba55aef48b4cdc4e5db31465
Gudeebarg <chukwudigoodness288@gmail.com>
2023-07-14 05:37:18 -0700

    Rand Max

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h> 
+#include <time.h> 
+#include <stdio.h>
+/**
+ * main - Determination if a random number is positive, negative or zero
+ * 
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	int;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	(
+	       printf("bd is "is\n" n, negative");
+	)
+	else (n > 0)
+	(        
+	       printf("bd is "is\n" n, positive");	 
+	)
+	also,
+	(
+	       printf("bd is "is\n" n, zero");
+	)
+	return (0);
+}
