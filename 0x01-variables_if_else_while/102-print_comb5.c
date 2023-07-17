d0bb6eb7d3803d7d42bdb1b3cfefcf40f1c8617a
Gudeebarg <chukwudigoodness288@gmail.com>
2023-07-17 08:01:03 -0700

    comma and space

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,29 @@
-#include < stdio.h >
+#include <stdio.h>
 
 /**
- * main - prints all possible combinations of two two-digit numbers
- * Return: Always 0 (Success)
+ *main - prints all possible combinations of two two-digit numbers
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
-		int i, j;
+	int i, j, k;
 
-			for (i = 0; i < 100; i++)
-					{
-								for (j = 0; j < 100; j++)
-											{
-															if (i < j)
-																			{
-																								putchar((i / 10) + 48);
-																												putchar((i % 10) + 48);
-																																putchar(' ');
-																																				putchar((j / 10) + 48);
-																																								putchar((j % 10) + 48);
-																																												if (i != 98 || j != 99)
-																																																	{
-																																																							putchar(',');
-																																																												putchar(' ');
-																																																																}
-																																															}
-
-																	}
-									}
-				putchar('\n');
-					return (0);
+	for (i = 0; i < 10; i++)
+	{
+	for (j = i + 1; j < 10; j++)
+	{
+	for (k = j + 1; k < 10; k++)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(k + '0');
+		if (i < 7)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
 }
-
-
