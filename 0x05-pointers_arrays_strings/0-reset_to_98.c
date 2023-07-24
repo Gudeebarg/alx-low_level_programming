13f7d65066e91611743f04219d650e4a2fa1562c
Gudeebarg <chukwudigoodness288@gmail.com>
2023-07-24 14:24:26 -0700

    battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,12 @@
 /**
  * reset_to_98 - a function that takes a pointer to an int as parameter
  * and updates the value it points to 98
- * @n: input
- * Return: n
+ *
+ * @n: the parameter
  */
+
 void reset_to_98(int *n);
 {
 	*n = 98;
+
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_
+#define MAIN_H_
 
-#include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 _putchar(char c);
 void reset_to_98(int *n);
